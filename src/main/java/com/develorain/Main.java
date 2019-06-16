98ed5d88268506431c22ce2375d560f3e1e77e7d
Develorain <321321893u213827@2312.com>
2019-06-16 15:56:57 -0400

    Updated main

--- src/main/java/com/develorain/Main.java
+++ src/main/java/com/develorain/Main.java
@@ -20,15 +20,8 @@ public class Main {
         BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("My2zlMkv4yorboQMABkUSqcNosJEqVZNi6JzPEvQovzbiVusGrf0ZkLF9rHkQAe7", "nUecuN1O33QAYXLdY76s12BME3fLafphBhj0kUl67Cs3seYxp8xzJ8JqVD7mYwJr");
         BinanceApiRestClient client = factory.newRestClient();
 
-        // Get all the asset codes of the currencies to determine how many nodes are in the graph
-        //int numberOfNodes = client.getAllAssets().size();
-
-
-        //client.getAllAssets().get(0).get
-
         SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
-
-
+        
         // Make nodes with each currency
         for (Asset asset : client.getAllAssets()) {
             String assetCode = asset.getAssetCode();
@@ -66,81 +59,3 @@ public class Main {
         System.out.println(johnsonSimpleCycles.findSimpleCycles());
     }
 }
-
-
-
-
-
-
-
-
-
-
-        /*
-        DirectedGraph directedGraph = new DirectedGraph(numberOfNodes);
-
-        // 0 = BTC
-        // 1 = ETH
-        // 2 = LTC
-        // 3 = BNB
-
-        // Connected BTC and ETH
-        directedGraph.addEdge(0, 1);
-        directedGraph.addEdge(1, 0);
-
-        // Connect ETH and LTC
-        directedGraph.addEdge(1, 2);
-        directedGraph.addEdge(2, 1);
-
-        // Connect LTC and BTC
-        directedGraph.addEdge(2, 0);
-        directedGraph.addEdge(0, 2);
-
-        // Connect BNB to BTC
-        directedGraph.addEdge(3, 0);
-
-        CycleFinder cycleFinder = new CycleFinder(directedGraph.getGraph());
-        */
-
-
-
-
-
-
-
-
-
-
-// Write all the prices to file code:
-/*
-List<TickerPrice> allPrices = client.getAllPrices();
-BufferedWriter writer = null;
-try {
-    writer = new BufferedWriter(new FileWriter("data.txt"));
-    writer.write(allPrices.toString());
-    writer.close();
-} catch (IOException e) {
-    e.printStackTrace();
-}
-*/
-
-
-// Read from file
-/*
-BufferedReader input = null;
-try {
-    input = new BufferedReader(new FileReader(new File("data.txt")));
-} catch (FileNotFoundException e) {
-    e.printStackTrace();
-}
-
-String line;
-
-try {
-    while ((line = input.readLine()) != null) {
-        System.out.println(line);
-    }
-} catch (Exception e) {
-    System.out.println("Mental boom");
-}
- */
